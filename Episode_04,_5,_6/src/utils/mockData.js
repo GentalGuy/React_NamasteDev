712568220cf4443c24aa024b27db3033304a0ac8
Gurkirat Singh <GurkiretSINGH@Mac-mini.local>
2025-05-22 23:12:39 -0400

    search added

---
+++ Episode_04,_5,_6/src/utils/mockData.js
@@ -0,0 +1,379 @@
+const resList = [
+  {
+    info: {
+      id: "254192",
+      name: "McDonald's",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/1/9/cb63e1b8-69b4-4974-a297-45d5439ff374_254192.JPG",
+      locality: "Sector 10",
+      areaName: "Rohini",
+      costForTwo: "₹400 for two",
+      cuisines: ["American"],
+      avgRating: 4.4,
+      parentId: "630",
+      avgRatingString: "4.4",
+      totalRatingsString: "23K+",
+      sla: {
+        deliveryTime: 23,
+        lastMileTravel: 2.3,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "2.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-05-23 00:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹49",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-de761995-84a8-4f5c-b7a5-459165697328",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/delhi/mcdonalds-sector-10-rohini-rest254192",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "228931",
+      name: "FreshMenu",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/3/5/6a631c47-9f7d-451e-8408-8c2a3fa32195_228931.jpg",
+      locality: "A Block",
+      areaName: "Rajouri Garden",
+      costForTwo: "₹250 for two",
+      cuisines: [
+        "Continental",
+        "Chinese",
+        "Oriental",
+        "Asian",
+        "Healthy Food",
+        "Fast Food",
+        "Indian",
+        "Desserts",
+      ],
+      avgRating: 4.1,
+      parentId: "398",
+      avgRatingString: "4.1",
+      totalRatingsString: "19K+",
+      sla: {
+        deliveryTime: 43,
+        lastMileTravel: 10.3,
+        serviceability: "SERVICEABLE",
+        slaString: "40-45 mins",
+        lastMileTravelString: "10.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-05-22 23:59:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹49",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-de761995-84a8-4f5c-b7a5-459165697328",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/delhi/freshmenu-a-block-rajouri-garden-rest228931",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "10208",
+      name: "Burger King",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2025/3/24/2991ca5f-8fa1-4ef7-87c7-d8406cdd5bba_10208.jpg",
+      locality: "Sector 10",
+      areaName: "Rohini",
+      costForTwo: "₹350 for two",
+      cuisines: ["Burgers", "American"],
+      avgRating: 4.3,
+      parentId: "166",
+      avgRatingString: "4.3",
+      totalRatingsString: "61K+",
+      sla: {
+        deliveryTime: 23,
+        lastMileTravel: 2.3,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "2.3 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-05-23 06:00:00",
+        opened: true,
+      },
+      badges: {},
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {},
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-de761995-84a8-4f5c-b7a5-459165697328",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/delhi/burger-king-sector-10-rohini-rest10208",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "555307",
+      name: "Chaayos Chai+Snacks=Relax",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/4/17/f92c604d-68cb-49db-8dec-9473650c363d_555307.JPG",
+      locality: "Sector-7",
+      areaName: "Rohini",
+      costForTwo: "₹250 for two",
+      cuisines: [
+        "Beverages",
+        "Chaat",
+        "Snacks",
+        "Bakery",
+        "Street Food",
+        "healthy",
+        "Home Food",
+        "Maharashtrian",
+        "Italian",
+        "Desserts",
+      ],
+      avgRating: 4.6,
+      parentId: "281782",
+      avgRatingString: "4.6",
+      totalRatingsString: "3.9K+",
+      sla: {
+        deliveryTime: 21,
+        lastMileTravel: 3,
+        serviceability: "SERVICEABLE",
+        slaString: "20-25 mins",
+        lastMileTravelString: "3.0 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-05-23 02:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Rxawards/_CATEGORY-Cafe%20&%20Chai.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId: "Rxawards/_CATEGORY-Cafe%20&%20Chai.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      aggregatedDiscountInfoV3: {
+        header: "ITEMS",
+        subHeader: "AT ₹199",
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "--",
+        },
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-de761995-84a8-4f5c-b7a5-459165697328",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/delhi/chaayos-chai-snacks-relax-sector-7-rohini-rest555307",
+      type: "WEBLINK",
+    },
+  },
+  {
+    info: {
+      id: "40415",
+      name: "Bikanervala",
+      cloudinaryImageId:
+        "RX_THUMBNAIL/IMAGES/VENDOR/2024/12/20/1f325fca-664b-4c47-853d-e16ad456808d_40415.jpg",
+      locality: "Rohini",
+      areaName: "Rohini",
+      costForTwo: "₹300 for two",
+      cuisines: ["North Indian", "Snacks", "Chaat", "Sweets"],
+      avgRating: 4.6,
+      parentId: "45936",
+      avgRatingString: "4.6",
+      totalRatingsString: "16K+",
+      sla: {
+        deliveryTime: 28,
+        lastMileTravel: 2.6,
+        serviceability: "SERVICEABLE",
+        slaString: "25-30 mins",
+        lastMileTravelString: "2.6 km",
+        iconType: "ICON_TYPE_EMPTY",
+      },
+      availability: {
+        nextCloseTime: "2025-05-23 00:00:00",
+        opened: true,
+      },
+      badges: {
+        imageBadges: [
+          {
+            imageId: "Green%20Dot%20Awards/Best%20In%20Chole%20Bhature.png",
+            description: "Delivery!",
+          },
+        ],
+      },
+      isOpen: true,
+      aggregatedDiscountInfoV2: {},
+      type: "F",
+      badgesV2: {
+        entityBadges: {
+          imageBased: {
+            badgeObject: [
+              {
+                attributes: {
+                  description: "Delivery!",
+                  imageId:
+                    "Green%20Dot%20Awards/Best%20In%20Chole%20Bhature.png",
+                },
+              },
+            ],
+          },
+          textBased: {},
+          textExtendedBadges: {},
+        },
+      },
+      differentiatedUi: {
+        displayType: "ADS_UI_DISPLAY_TYPE_ENUM_DEFAULT",
+        differentiatedUiMediaDetails: {
+          lottie: {},
+          video: {},
+        },
+      },
+      reviewsSummary: {},
+      displayType: "RESTAURANT_DISPLAY_TYPE_DEFAULT",
+      restaurantOfferPresentationInfo: {},
+      externalRatings: {
+        aggregatedRating: {
+          rating: "4.3",
+          ratingCount: "39",
+        },
+        source: "GOOGLE",
+        sourceIconImageId: "v1704440323/google_ratings/rating_google_tag",
+      },
+      ratingsDisplayPreference: "RATINGS_DISPLAY_PREFERENCE_SHOW_SWIGGY",
+    },
+    analytics: {
+      context: "seo-data-de761995-84a8-4f5c-b7a5-459165697328",
+    },
+    cta: {
+      link: "https://www.swiggy.com/city/delhi/bikanervala-rohini-rest40415",
+      type: "WEBLINK",
+    },
+  },
+];
+
+export default resList;
